4ac2f5488c337864fad3b9e129379be4ad9e94fb
Mateusz <mat646@gmail.com>
2019-04-23 22:37:10 +0200

    Implemented AccountFactory

--- Middleware/bank/server.py
+++ Middleware/bank/server.py
@@ -4,15 +4,14 @@ import Ice
 import signal
 import grpc
 
+sys.path.append(os.path.abspath("./utils/out/proto"))
+
 import exchange_pb2
 import exchange_pb2_grpc
 
-sys.path.append(os.path.abspath("./utils/out"))
+sys.path.append(os.path.abspath("./utils/out/ice"))
 
 from BankSystem import *
-
-#class Exchange(exchange_pb2_grpc.ExchangeServicer):
-#  def subscribeExchangeRate(self, request, context):
 
 def run():
   channel = grpc.insecure_channel('localhost:50051')
@@ -20,19 +19,55 @@ def run():
   for response in stub.subscribeExchangeRate(exchange_pb2.ExchangeRequest()):
     print("Greeter client received: " + str(response.currency))
 
-class BankI(Bank):
+
+class InvalidAccountTypeExceptionI(InvalidAccountTypeException):
+  pass
+
+
+class InvalidCredentialsExceptionI(InvalidCredentialsException):
+  pass
+
+
+class AccountI(Account):
+  def __init__(self, accountType, name, surname, pesel, password, income):
+    self.accountType = accountType
+    self.name = name
+    self.surname = surname
+    self.pesel = pesel
+    self.password = password
+    self.income = income
+    self.balance = income + 100
+
+  def getAccountType(self, current):
+    return self.accountType
+
+  def getAccountBalance(self, current):
+    return self.balance
+
+  def applyForCredit(self, currency, amount, period, current):
+    if self.accountType == AccountType.STANDARD:
+      raise Exception('blalba')
+    return CreditEstimate(Balance(1000), Balance(100))
+
+
+class AccountFactoryI(AccountFactory):
   def createAccount(self, name, surname, pesel, income, current):
-    print("XD server")
-    return AccountCreated(Password("wed3rgeo"))
+    accType = AccountType.STANDARD
+    if income.value > 1000:
+      accType = AccountType.PREMIUM
+    return AccountCreated(Password("wed3rgeo"), accType)
+  def obtainAccess(self, credentials, current):
+    #TODO validation bla bla... adapter goes up
+    return AccountPrx.checkedCast(current.adapter.add(AccountI(), Ice.stringToIdentity(str(pesel))))
     
 
 with Ice.initialize(sys.argv, "./bank/config.server") as communicator:
   
-  run()
-  print("xd")
+  #run()
   signal.signal(signal.SIGINT, lambda signum, frame: communicator.shutdown())
-  adapter = communicator.createObjectAdapter("Bank")
-  adapter.add(BankI(), Ice.stringToIdentity("bank"))
+  adapter = communicator.createObjectAdapter("AccountFactory")
+  adapter.add(AccountFactoryI(), Ice.stringToIdentity("accountFactory"))
   adapter.activate()
   communicator.waitForShutdown()
+
 
--- Middleware/utils/bank.ice
+++ Middleware/utils/bank.ice
@@ -13,7 +13,6 @@ module BankSystem {
 
   struct AccountCreated { Password password; AccountType accountType; };
   struct Credentials { Pesel pesel; Password password; };
-  
   struct CreditEstimate { Balance originCurrency; Balance foreignCurrency; };
 
   exception InvalidCredentialsException {
@@ -24,11 +23,16 @@ module BankSystem {
     string reason = "credits are only for premium users";
   };
 
-  interface Bank {
-    AccountCreated createAccount(Name name, Surname surname, Pesel pesel, Balance income);
-    Balance accountBalance(Credentials credentials) throws InvalidCredentialsException;
-    CreditEstimate applyForCredit(Credentials credentials, Currency currency, Balance amount, Period period) throws InvalidAccountTypeException;
+  interface Account {
+    AccountType getAccountType();
+    Balance getAccountBalance();
+    CreditEstimate applyForCredit(Currency currency, Balance amount, Period period) throws InvalidAccountTypeException;
   };
 
+  interface AccountFactory {
+    AccountCreated createAccount(Name name, Surname surname, Pesel pesel, Balance income);
+    Account* obtainAccess(Credentials credentials) throws InvalidCredentialsException;
+  };
+  
 };
 
