65d10f82b102b066837f0499904951e68e41f166
Mateusz <mat646@gmail.com>
2019-04-30 13:12:37 +0200

    Implemented AccountFactory

--- Middleware/bank/server.py
+++ Middleware/bank/server.py
@@ -13,61 +13,63 @@ sys.path.append(os.path.abspath("./utils/out/ice"))
 
 from BankSystem import *
 
+
 def run():
-  channel = grpc.insecure_channel('localhost:50051')
-  stub = exchange_pb2_grpc.ExchangeStub(channel)
-  for response in stub.subscribeExchangeRate(exchange_pb2.ExchangeRequest()):
-    print("Greeter client received: " + str(response.currency))
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = exchange_pb2_grpc.ExchangeStub(channel)
+    for response in stub.subscribeExchangeRate(exchange_pb2.ExchangeRequest()):
+        print("Greeter client received: " + str(response.currency))
 
 
 class InvalidAccountTypeExceptionI(InvalidAccountTypeException):
-  pass
+    pass
 
 
 class InvalidCredentialsExceptionI(InvalidCredentialsException):
-  pass
+    pass
 
 
 class AccountI(Account):
-  def __init__(self, accountType, name, surname, pesel, password, income):
-    self.accountType = accountType
-    self.name = name
-    self.surname = surname
-    self.pesel = pesel
-    self.password = password
-    self.income = income
-    self.balance = income + 100
+    def __init__(self, accountType, name, surname, pesel, password, income):
+        self.accountType = accountType
+        self.name = name
+        self.surname = surname
+        self.pesel = pesel
+        self.password = password
+        self.income = income
+        self.balance = Balance(income.value + 100)
 
-  def getAccountType(self, current):
-    return self.accountType
+    def getAccountType(self, current):
+        return self.accountType
 
-  def getAccountBalance(self, current):
-    return self.balance
+    def getAccountBalance(self, current):
+        return self.balance
 
-  def applyForCredit(self, currency, amount, period, current):
-    if self.accountType == AccountType.STANDARD:
-      raise Exception('blalba')
-    return CreditEstimate(Balance(1000), Balance(100))
+    def applyForCredit(self, currency, amount, period, current):
+        if self.accountType == AccountType.STANDARD:
+            raise Exception('blalba')
+        return CreditEstimate(Balance(1000), Balance(100))
 
 
 class AccountFactoryI(AccountFactory):
-  def createAccount(self, name, surname, pesel, income, current):
-    accType = AccountType.STANDARD
-    if income.value > 1000:
-      accType = AccountType.PREMIUM
-    return AccountCreated(Password("wed3rgeo"), accType)
-  def obtainAccess(self, credentials, current):
-    #TODO validation bla bla... adapter goes up
-    return AccountPrx.checkedCast(current.adapter.add(AccountI(), Ice.stringToIdentity(str(pesel))))
-    
+    def createAccount(self, name, surname, pesel, income, current):
+        accType = AccountType.STANDARD
+        if income.value > 1000:
+            accType = AccountType.PREMIUM
+        password = Password("wed3rgeo")
+        account = AccountI(accType, name, surname, pesel, password, income)
+        current.adapter.add(account, Ice.stringToIdentity(str(pesel.value)))
+        return AccountCreated(Password("wed3rgeo"), accType)
+
+    def obtainAccess(self, credentials, current):
+        # TODO validation bla bla...
+        return AccountPrx.checkedCast(current.adapter.createProxy(Ice.stringToIdentity(str(credentials.pesel.value))))
+
 
 with Ice.initialize(sys.argv, "./bank/config.server") as communicator:
-  
-  #run()
-  signal.signal(signal.SIGINT, lambda signum, frame: communicator.shutdown())
-  adapter = communicator.createObjectAdapter("AccountFactory")
-  adapter.add(AccountFactoryI(), Ice.stringToIdentity("accountFactory"))
-  adapter.activate()
-  communicator.waitForShutdown()
-
-
+    # run()
+    signal.signal(signal.SIGINT, lambda signum, frame: communicator.shutdown())
+    adapter = communicator.createObjectAdapter("AccountFactory")
+    adapter.add(AccountFactoryI(), Ice.stringToIdentity("accountFactory"))
+    adapter.activate()
+    communicator.waitForShutdown()
--- Middleware/client/client.py
+++ Middleware/client/client.py
@@ -2,16 +2,16 @@ import sys
 import os
 import Ice
 
-sys.path.append(os.path.abspath("./utils/out"))
+sys.path.append(os.path.abspath("./utils/out/ice"))
 
 from BankSystem import *
 
 with Ice.initialize(sys.argv, "./client/config.client") as communicator:
-  twoway = AccountFactoryPrx.checkedCast(
-    communicator.propertyToProxy('AccountFactory.Proxy').ice_twoway().ice_secure(False))
-  if not twoway:
-    print("invalid proxy")
-    sys.exit(1)
-  xd = twoway.createAccount(Name("xd"), Surname("xd"), Pesel(12), Balance(12))
-  sth = twoway.obtainAccess(Credentials(Pesel(12), xd.password))
-  print(xd)
+    twoway = AccountFactoryPrx.checkedCast(
+        communicator.propertyToProxy('AccountFactory.Proxy').ice_twoway().ice_secure(False))
+    if not twoway:
+        print("invalid proxy")
+        sys.exit(1)
+    xd = twoway.createAccount(Name("xd"), Surname("xd"), Pesel(12), Balance(12))
+    sth = twoway.obtainAccess(Credentials(Pesel(12), xd.password))
+    print(xd)
